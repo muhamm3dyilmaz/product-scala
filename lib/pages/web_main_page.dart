5f7c6d6275a86e591f5cf2fc6a2c0040f895ece5
Muhammed Yılmaz <mylmz2101@gmail.com>
2023-05-04 15:26:28 +0300

    Muhammed Ilk Push

---
+++ lib/pages/web_main_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WebMainPage extends StatefulWidget {
+  const WebMainPage({super.key});
+
+  @override
+  State<WebMainPage> createState() => _WebMainPageState();
+}
+
+class _WebMainPageState extends State<WebMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Deneme")),
+    );
+  }
+}
